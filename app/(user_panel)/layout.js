03b63da7c0323aa68f52edd858d16dca01f1ada1
Bünyamin Korkut <bunyaminkrkt@hotmail.com>
2023-07-02 15:37:00 +0300

    fix imports for build

--- app/(user_panel)/layout.js
+++ app/(user_panel)/layout.js
@@ -1,6 +1,6 @@
 import PanelLayout from "@/components/layouts/panelLayout";
-import DeleteModal from "@/components/modals/deleteModal";
-import StudentModal from "@/components/modals/studentModal";
+import DeleteModal from "@/components/modals/DeleteModal";
+import StudentModal from "@/components/modals/StudentModal";
 
 export default function RootLayout({
   children,
